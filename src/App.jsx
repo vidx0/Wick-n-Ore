992ee2b9556310358ef6b717ee47ab2c9c007a8a
22muppasa <98856260+22muppasa@users.noreply.github.com>
2024-06-14 14:39:10 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import Black from "./components/Black.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Module1 from "./components/Module1.jsx";
 import Module2 from "./components/Module2.jsx";
 import Module3 from "./components/Module3.jsx";
+import ScrollToTopButton from "./components/ScrollToTopButton.jsx";
 import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Module1 />
         <Module2 />
         <Module3 />
+        <ScrollToTopButton />
       </ParallaxProvider>
     </>
   );
